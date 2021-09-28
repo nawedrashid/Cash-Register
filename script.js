52687a4e5d76b878a5921170906e76101c465b2d
nawedrashid <rashidnawed@gmail.com>
2021-09-28 16:25:51 +0530

    completed html css and js

---
+++ script.js
@@ -0,0 +1,37 @@
+const BillAmount = document.querySelector("#bill-amount");
+const CashGiven = document.querySelector("#cash-given");
+const CheckButton = document.querySelector("#check-btn");
+const message = document.querySelector(".error");
+const noOfNotes = document.querySelectorAll(".note-count");
+const Output = document.querySelector(".output")
+
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+CheckButton.addEventListener("click", function Validate(){
+    hideMessage();
+    if(BillAmount.value > 0){
+        if(CashGiven.value >= BillAmount.value){
+            const AmounttobeReturned = CashGiven.value - BillAmount.value;
+            calculateChange(AmounttobeReturned);
+            Output.style.display="block";
+        }else{
+            showMessage("Insufficient Cash");
+        }
+    }else{
+        showMessage("Invalid Bill Amount");
+    }
+});
+function showMessage(msg){
+    message.style.display="block";
+    message.innerText=msg;
+}
+function hideMessage(){;
+    message.style.display="none";
+}
+function calculateChange(AmounttobeReturned){
+    for(let i=0;i<availableNotes.length;i++){
+        const numberOfNotes = Math.trunc(AmounttobeReturned/availableNotes[i]);
+        AmounttobeReturned = AmounttobeReturned%availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+}
